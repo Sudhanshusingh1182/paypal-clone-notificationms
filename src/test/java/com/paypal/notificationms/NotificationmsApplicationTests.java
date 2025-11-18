411e3b6332ad2ccd5611d46b5854a5fc6f35429c
Sudhanshu Singh <sudhanshu1182abc@gmail.com>
2025-11-18 23:12:43 +0530

    Initial files

---
+++ src/test/java/com/paypal/notificationms/NotificationmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paypal.notificationms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
